a215824ce6b5b45383b8a3c5d5984d3784db037c
Ekoll <kurniae047@email.com>
2025-06-25 22:30:39 +0700

    tambah fitur berita dan shorts

--- resources/views/web-berita.blade.php
+++ resources/views/web-berita.blade.php
@@ -4,22 +4,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Hypenings - Media Berita</title>
     <link rel="stylesheet" href="{{ asset('css/web-berita.css') }}">
 </head>
 
 <body>
     <div class="slider-wrapper">
         <nav class="navbar">
+
             <div class="logo">
                 <img src="{{ asset('image/hype.png') }}" alt="logo" width="120">
             </div>
 
+            <input type="checkbox" id="menu-toggle">
+            <label for="menu-toggle" class="hamburger">☰</label>
+
+            <aside class="offcanvas">
+                <label for="menu-toggle" class="close-btn">×</label>
+                <nav>
+                    <h3>Menu</h3>
+                    <a href="#">Trending</a>
+                    <a href="#">Topik</a>
+                    <a href="#topik">Category</a>
+                    <a href="#">Shorts</a>
+                    <a href="#">Tentang Kita?</a>
+                </nav>
+            </aside>
+
             <ul class="nav-links">
-                <li><a href="">Beranda</a></li>
-                <li><a href="">Trending</a></li>
-                <li><a href="">Topik</a></li>
-                <li><a href="">Shorts</a></li>
+                <li><a href="#trending">Trending</a></li>
+                <li><a href="#topik">Topik</a></li>
+                <li><a href="#topik">Category</a></li>
+                <li><a href="#shorts">Shorts</a></li>
                 <li><a href="">Tentang Kita?</a></li>
             </ul>
         </nav>
@@ -36,48 +52,51 @@
         </div>
     </div>
 
-    <section class="section">
-        <h2>Berita Tren</h2>
+    <section class="section" id="trending">
+        <h2 class="section-title">Berita Tren</h2>
         <div class="container">
 
             <!-- Card 1 -->
-            <div class="card">
+            <article class="card">
                 <img src="{{ asset('image/iseo.jpg') }}" alt="">
                 <div class="content">
                     <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     <a href="#">Baca Selengkapnya →</a>
                 </div>
-            </div>
+            </article>
 
             <!-- Card 2 -->
-            <div class="card">
+            <article class="card">
                 <img src="{{ asset('image/iseo.jpg') }}" alt="">
                 <div class="content">
                     <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     <a href="#">Baca Selengkapnya →</a>
                 </div>
-            </div>
+            </article>
 
-            <!-- Card 2 -->
-            <div class="card">
+            <!-- Card 3 -->
+            <article class="card">
                 <img src="{{ asset('image/iseo.jpg') }}" alt="">
                 <div class="content">
                     <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     <a href="#">Baca Selengkapnya →</a>
                 </div>
-            </div>
+            </article>
+
+        </div>
     </section>
 
-    <section class="news-section">
+
+    <section class="news-section" id="topik">
         <div class="news-headline-container">
-            <h2 class="section-title">TERBARU</h2>
+            <h2 class="section-title">TOPIK</h2>
             <h2 class="news-section-title">TERPOPULER</h2>
         </div>
         <div class="latest-news-container">
             <div class="news-cards-wrapper">
 
                 <!-- News Card 1 -->
-                <div class="news-cards">
+                <article class="news-cards">
                     <img src="{{ asset('image/iseo.jpg') }}" alt="thumb" class="thumb">
                     <div class="news-contents">
                         <span class="category">Lorem Ipsum</span>
@@ -85,10 +104,10 @@
                         <p class="date">11 Juni 2025, 17:36 WIB</p>
                         <button>Selengkapnya</button>
                     </div>
-                </div>
+                </article>
 
                 <!-- News Card 2 -->
-                <div class="news-cards">
+                <article class="news-cards">
                     <img src="{{ asset('image/iseo.jpg') }}" alt="">
                     <div class="news-contents">
                         <span class="category">Lorem ipsum</span>
@@ -96,10 +115,10 @@
                         <p>11 Juni 2025, 17:36 WIB</p>
                         <button>Selengkapnya</button>
                     </div>
-                </div>
+                </article>
 
                 <!-- News Card 3 -->
-                <div class="news-cards">
+                <article class="news-cards">
                     <img src="{{ asset('image/iseo.jpg') }}" alt="">
                     <div class="news-contents">
                         <span class="category">Lorem Ipsum</span>
@@ -107,10 +126,10 @@
                         <p>11 Juni 2025, 17:36 WIB</p>
                         <button>Selengkapnya</button>
                     </div>
-                </div>
+                </article>
 
                 <!-- News Card 4 -->
-                <div class="news-cards">
+                <article class="news-cards">
                     <img src="{{ asset('image/iseo.jpg') }}" alt="">
                     <div class="news-contents">
                         <span class="category">Lorem Ipsum</span>
@@ -118,10 +137,10 @@
                         <p>11 Juni 2025, 17:36 WIB</p>
                         <button>Selengkapnya</button>
                     </div>
-                </div>
+                </article>
             </div>
 
-            <div class="list">
+            <div class="list" id="terpopuler">
                 <!-- List Item -->
                 <div class="item">
                     <div class="number">1</div>
@@ -159,19 +178,76 @@
         </div>
     </section>
 
+    <section class="shorts-section" id="shorts">
+        <h2 class="shorts-title">HYPENINGS SHORTS</h2>
+        <div class="shorts-container">
+
+            <!-- Card Shorts 1-->
+            <article class="short-card">
+                <div class="thumb-wrapper">
+                    <img src="{{ asset('image/iseo.jpg') }}" alt="thumbnail">
+                    <div class="play-button">▶</div>
+                </div>
+                <div class="short-desc">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+            </article>
+
+
+            <!-- Card Shorts 2-->
+            <article class="short-card">
+                <div class="thumb-wrapper">
+                    <img src="{{ asset('image/iseo.jpg') }}" alt="thumbnail">
+                    <div class="play-button">▶</div>
+                </div>
+                <div class="short-desc">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+            </article>
+
+
+            <!-- Card Shorts 3 -->
+            <article class="short-card">
+                <div class="thumb-wrapper">
+                    <img src="{{ asset('image/iseo.jpg') }}" alt="thumbnail">
+                    <div class="play-button">▶</div>
+                </div>
+                <div class="short-desc">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+            </article>
+
+
+            <!-- Card Shorts 4 -->
+            <article class="short-card">
+                <div class="thumb-wrapper">
+                    <img src="{{ asset('image/iseo.jpg') }}" alt="thumbnail">
+                    <div class="play-button">▶</div>
+                </div>
+                <div class="short-desc">
+                    <p>Lorem Ipsum dolor sit amet cos=nsectetur adipisicing elit.</p>
+                </div>
+            </article>
+        </div>
+
+        <div class="see-all">
+            <a href="#">Lihat Semua &rarr;</a>
+        </div>
+    </section>
+
 
     <footer class="footer">
         <p>© 2025 Stay curious. Hypenings.</p>
         <p>FOLLOW KITA!</p>
         <div class="social-icons">
             <a href="https://instagram.com/" target="_blank">
-            <img src="https://cdn-icons-png.flaticon.com/512/174/174855.png" alt="Instagram" />
+                <img src="https://cdn-icons-png.flaticon.com/512/174/174855.png" alt="Instagram" />
             </a>
             <a href="https://www.tiktok.com/" target="_blank">
-            <img src="https://cdn-icons-png.flaticon.com/512/3046/3046121.png" alt="TikTok" />
+                <img src="https://cdn-icons-png.flaticon.com/512/3046/3046121.png" alt="TikTok" />
             </a>
             <a href="https://facebook.com/" target="_blank">
-            <img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Facebook" />
+                <img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Facebook" />
             </a>
         </div>
     </footer>
